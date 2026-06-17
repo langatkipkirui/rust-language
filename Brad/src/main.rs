cf93ffa47f20e87824c16de82cdd7a0287961c79
Kevin Langat <kevinlangat419@gmail.com>
2026-06-17 23:12:43 +0300

    Learned basic formatting e.g using Number:{}, 1; to fill dynamic values

--- Brad/src/main.rs
+++ Brad/src/main.rs
@@ -3,4 +3,8 @@ mod print;
 fn main() {
     // println!("Hello, world!");
     print::run();
+
+    // formatting
+    println!("Number: {}", 1);
+    println!("{} is from {}", "Kevin","Kericho");
 }
