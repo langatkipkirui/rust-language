879b14d8934b8582eb966ee739bf31fb53f36c29
Kevin Langat <kevinlangat419@gmail.com>
2026-06-19 23:35:24 +0300

    did the placeholder for debug trait and basic math println code

--- Brad/src/main.rs
+++ Brad/src/main.rs
@@ -6,14 +6,20 @@ fn main() {
 
     // formatting
     println!("Number: {}", 1);
-    println!("{} is from {}", "Kevin","Kericho");
+    println!("{} is from {}", "Kevin", "Kericho");
 
     // Positional arguments
-    println!("{0} is from {1} and {0} likes to {2}", {"Kevin"},{"Kericho"}, {"code"} );
+    println!("{0} is from {1} and {0} likes to {2}", { "Kevin" }, { "Kericho" }, { "code" });
 
     // name arguments
-    println!("{name} likes to play {activity}", name="John", activity="Baseball");
+    println!("{name} likes to play {activity}", name = "John", activity = "Baseball");
 
     // Placeholder Traits
-    println!("Binary {:b} Hex: {:x} Octal: {:0}", 10,10,10);
+    println!("Binary {:b} Hex: {:x} Octal: {:0}", 10, 10, 10);
+
+    // placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+
+    // Basic math
+    println!("10+10={}", 10 + 10);
 }
