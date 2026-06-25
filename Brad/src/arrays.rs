d9a7037f647a545c489d541be600d935ced82251
Kevin Langat <kevinlangat419@gmail.com>
2026-06-25 23:22:43 +0300

    how to get the array length and the bytes it occupies

--- Brad/src/arrays.rs
+++ Brad/src/arrays.rs
@@ -1,10 +1,15 @@
+use std::mem;
 // fixed list where elements are the same data types
 pub fn run() {
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    let numbers: [i32; 4] = [1, 2, 3, 4];
     println!("{:?}", numbers);
 
     // get a single value
     println!("single value:{}", numbers[2]);
 
     // get the length of the array
+    println!("Array length: {}", numbers.len());
+
+    // Arrays are stack allocated
+    println!("Array occupies {} bytes", mem::size_of_val(&numbers))
 }
