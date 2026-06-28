2384ff33e4c36d6762806b191d2da479baa629bd
Kevin Langat <kevinlangat419@gmail.com>
2026-06-28 22:54:07 +0300

    try to understood closures

--- Brad/src/functions.rs
+++ Brad/src/functions.rs
@@ -1,8 +1,12 @@
 pub fn run() {
     greeting("Hello", "Kevin");
-
+    // Bind a function to variables
     let get_sum = add(10, 5);
     println!("Sum: {}", get_sum);
+    // Closure
+    let n3 = 10;
+    let add_nums = |n1: i32, n2: i32| n1 + n2 + n3;
+    println!("C Sum: {}", add_nums(3, 3));
 }
 
 fn greeting(greet: &str, name: &str) {
