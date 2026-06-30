d8b5214a924ebf3d66b722c4d7458c2888010d95
Kevin Langat <kevinlangat419@gmail.com>
2026-06-30 22:25:07 +0300

    learned a little about &self in functions params

--- Brad/src/structs.rs
+++ Brad/src/structs.rs
@@ -4,6 +4,23 @@ struct Color {
     green: u8,
     blue: u8,
 }
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
 pub fn run() {
     let mut c = Color {
         red: 255,
@@ -15,12 +32,17 @@ pub fn run() {
     c.blue = 2;
 
     // tuple struct
-    struct Color_2(u8, u8, u8);
+    struct Colorsec(u8, u8, u8);
 
     println!("Color: {} {} {}", c.red, c.green, c.blue);
 
-    let mut c = Color_2(255, 0, 0);
+    let mut c = Colorsec(255, 0, 0);
     c.0 = 200;
 
-    println!("Color: {} {} {}", c.0, c.1, c.2)
+    println!("Color: {} {} {}", c.0, c.1, c.2);
+
+    let p = Person::new("John", "Doe");
+
+    println!("person {} {}", p.first_name, p.last_name);
+    println!("Person full name: {}", p.full_name());
 }
