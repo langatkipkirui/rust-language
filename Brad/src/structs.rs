135016c1c8d86825432d38297409337f932245d3
Kevin Langat <kevinlangat419@gmail.com>
2026-06-30 21:56:29 +0300

    Did an intro to tuple struct

--- Brad/src/structs.rs
+++ Brad/src/structs.rs
@@ -14,5 +14,13 @@ pub fn run() {
     // we can directly mutate the struct
     c.blue = 2;
 
+    // tuple struct
+    struct Color_2(u8, u8, u8);
+
     println!("Color: {} {} {}", c.red, c.green, c.blue);
+
+    let mut c = Color_2(255, 0, 0);
+    c.0 = 200;
+
+    println!("Color: {} {} {}", c.0, c.1, c.2)
 }
