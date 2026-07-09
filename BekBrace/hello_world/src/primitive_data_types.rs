25ead05ae3fbf81f840abb4b5c0fc56c17525b80
Kevin Langat <kevinlangat419@gmail.com>
2026-07-09 15:58:00 +0300

    initial commit of bekbrace

---
+++ BekBrace/hello_world/src/primitive_data_types.rs
@@ -0,0 +1,7 @@
+// primitve data types
+// int, float, char
+
+// Integer
+// Rust has signed (+ and -) and usigned integer(onl+) types of different sizes.
+// i8, i16, i32 i64, i128: signed integers
+
