d309b7cc8f1dddfb4999cada1c3201f881075a33
Kevin Langat <kevinlangat419@gmail.com>
2026-07-10 23:44:12 +0300

    understood a little more about tuples and mixed tuples

--- BekBrace/hello_world/src/arrays.rs
+++ BekBrace/hello_world/src/arrays.rs
@@ -2,6 +2,7 @@
 // arrays, tuples, slices, and strings (slice string)
 
 pub fn run() {
+    // arrays
     let numbers: [i32; 5] = [1, 2, 3, 4, 5];
     println!("An array: {:?}", numbers);
 
@@ -10,4 +11,12 @@ pub fn run() {
     println!("Fruits array 1st element: {}", fruits[0]);
     println!("Fruits array 2nd element: {}", fruits[1]);
     println!("Fruits array 3rd element: {}", fruits[2]);
+
+    // tuples
+    let human = ("Alice", 30, false);
+    println!("Human tuple {:?}", human);
+
+    // --> mix tuples
+    let my_mix_tuple = ("Kratos", 23, true, [1, 2, 3, 4, 5]);
+    println!("My mix tuple: {:?}", my_mix_tuple)
 }
