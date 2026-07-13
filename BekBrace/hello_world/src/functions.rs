090ceeea95cdc8441d9317eb513a1bd0bf6413f0
Kevin Langat <kevinlangat419@gmail.com>
2026-07-13 16:20:29 +0300

    functions can take multiple parameters

--- BekBrace/hello_world/src/functions.rs
+++ BekBrace/hello_world/src/functions.rs
@@ -9,5 +9,11 @@ pub fn run() {
 }
 
 fn tell_height(height: i32) {
-    println!("My height is  {}cm", height)
+    // println!("My height is  {}cm", height)
+    human_id("Kevin", 22, 182.0);
+}
+
+//  you can insert more than on e parameter
+fn human_id(name: &str, age: u32, height: f32) {
+    print!("My name is {}, I am {} years old and my height is {}", name, age, height)
 }
