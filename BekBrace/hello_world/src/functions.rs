8b21fd403098be1dfcedb7f73167b8142ede264c
Kevin Langat <kevinlangat419@gmail.com>
2026-07-13 16:31:03 +0300

    Did a little about expressions and returning them

--- BekBrace/hello_world/src/functions.rs
+++ BekBrace/hello_world/src/functions.rs
@@ -10,10 +10,20 @@ pub fn run() {
 
 fn tell_height(height: i32) {
     // println!("My height is  {}cm", height)
-    human_id("Kevin", 22, 182.0);
+    let x = {
+        let price = 5;
+        let qty = 10;
+        price * qty
+    };
+    human_id("Kevin", 22, 182.0, x);
 }
 
 //  you can insert more than on e parameter
-fn human_id(name: &str, age: u32, height: f32) {
-    print!("My name is {}, I am {} years old and my height is {}", name, age, height)
+fn human_id(name: &str, age: u32, height: f32, x: i32) {
+    println!("My name is {}, I am {} years old and my height is {}", name, age, height);
+    println!("The X value is {}", x);
 }
+
+// Expressions and statements
+// An expression is anything that returns a Value
+// A statement is anything that does not return a value
